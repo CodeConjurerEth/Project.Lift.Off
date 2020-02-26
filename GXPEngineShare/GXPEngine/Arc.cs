bf6e96326fcac306025b62a48615f0a1869f3038
BobSter <miu.vlad00@gmail.com>
2020-02-26 20:09:07 +0100

    arc

--- GXPEngineShare/GXPEngine/Arc.cs
+++ GXPEngineShare/GXPEngine/Arc.cs
@@ -3,8 +3,23 @@ using GXPEngine;
 
 public class Arc : Sprite
 {
+    private Sprite _line;
+
     public Arc() : base("Arc.png")
     {
+        this.scale = 0.5f;
+        this.SetOrigin(this.width, this.height/4);
 
+        _line = new Sprite("square.png");
+        _line.SetOrigin(_line.width / 2, _line.height);
+        _line.y = this.y-200;
+        _line.x = this.x;
+        _line.scale = 3;
+        AddChild(_line);
+    }
+
+    public void Update()
+    {
+        
     }
 }
