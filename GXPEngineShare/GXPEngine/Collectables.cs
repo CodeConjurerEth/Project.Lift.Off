ac6f1f280932daaac6577a0545b9a19141e2b4bb
xLynnx2 <60228945+xLynnx2@users.noreply.github.com>
2020-02-27 16:12:34 +0100

    put animations in

--- GXPEngineShare/GXPEngine/Collectables.cs
+++ GXPEngineShare/GXPEngine/Collectables.cs
@@ -20,24 +20,10 @@ public class Collectables : AnimationSprite
         this.x = _startX;
         this.y = _startY;
 
-<<<<<<< HEAD
+
         scale = 1.8f;
 
 
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-        //scale = .8f;
-=======
-<<<<<<< HEAD
-        scale = 1.8f;
-=======
->>>>>>> c2fa3e3c1e6c203e3d3b2f0ef4d784e6ec06e247
-        scale = 4.8f;
->>>>>>> d60d7bbc6277040e639965b99dae8cbfdc854620
-
->>>>>>> 8bec5610d0f70359398fa4d6920939c9e2eb1fb7
         Timer timer = new Timer(5000, LateDestroy);
         AddChild(timer);
 
