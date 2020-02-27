36d5f0a4fcf2a5d03955cc6ba00038f6214b1ba2
xLynnx2 <60228945+xLynnx2@users.noreply.github.com>
2020-02-27 14:40:38 +0100

    Put all the right backgrounds in

--- GXPEngineShare/GXPEngine/Collectables.cs
+++ GXPEngineShare/GXPEngine/Collectables.cs
@@ -6,21 +6,21 @@ public class Collectables : AnimationSprite
     private float _speedX = 0.7f;
     private float _speedY = 5f;
 
-    private int numberOffFrames = 7;
+    private int numberOffFrames = 6;
     private int startFrame = 0;
-    private float frameInterval = 90f;
+    private float frameInterval = 200f;
 
     private float _animationTimer = 0;
 
  
 
 
-    public Collectables(int _startX, int _startY) : base("barry.png", 7, 1)
+    public Collectables(int _startX, int _startY) : base("collectable.png", 4, 2)
     {
         this.x = _startX;
         this.y = _startY;
 
-        scale = 1.8f;
+        scale = 4.8f;
 
         Timer timer = new Timer(5000, LateDestroy);
         AddChild(timer);
